c09e1afac351a5784b7c8c89ef7f36b23ef251ba
pcl-aacin <3304457468@qq.com>
2024-09-21 23:59:52 +0800

    #

--- lib/router.dart
+++ lib/router.dart
@@ -4,7 +4,7 @@ import "./screens/home/main.dart";
 import "./screens/game/main.dart";
 
 final List<GoRoute> routes = [
-	GoRoute(path: "/", redirect: (ctx, _) => "/game"),
+	GoRoute(path: "/", redirect: (ctx, _) => "/home"),
 	GoRoute(path: "/home", builder: (ctx, _) => const HomeScreen()),
 	GoRoute(path: "/game", builder: (ctx, _) => const GameScreen()),
 ];
